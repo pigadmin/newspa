5730d6cdbb7e5ddc118082c7c77b9d3206cb68da
635488410@qq.com <635488410@qq.com>
2018-08-20 10:22:12 +0800

    11

--- app/src/main/java/com/spa/ui/bottom/OrderFr.java
+++ app/src/main/java/com/spa/ui/bottom/OrderFr.java
@@ -1,27 +1,21 @@
 package com.spa.ui.bottom;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.spa.R;
-import com.spa.app.App;
 import com.spa.ui.BaseFr;
 
 public class OrderFr extends BaseFr {
 
     private View view;
-    private Activity activity;
-    private App app;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_order, container, false);
-        activity = getActivity();
-        app = (App) activity.getApplication();
         return view;
     }
 }
